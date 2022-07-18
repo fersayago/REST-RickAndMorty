7677bbf712c2c2afe25f5acd6b10f4b3faa8d4c6
fersayago <sayago.fernando@gmail.com>
2022-07-18 01:32:55 -0300

    deployment config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://fersayago.github.io/REST-RickAndMorty/",
   plugins: [react()]
 })
